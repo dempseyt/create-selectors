964417dbcb629511ae2bacb4d60d3686b12b591a
dempseyt <dempsey.thompson@journeyone.com.au>
2024-04-24 15:21:02 +0800

    GREEN: create-selectors.js simple properties creates a selector for a simple property with a different root

--- src/createSelectors.js
+++ src/createSelectors.js
@@ -13,12 +13,14 @@ function createSelectors(selectorSpec) {
 
   for (const [key, value] of Object.entries(selectorSpec)) {
     if (value["_export"] !== false) {
-      selectors[createSelectorName(key)] = (state) => {
+      const selectorFunction = (_state) => {
+        const state = selectors.selectState(_state);
         if (!Object.hasOwn(state, key) && Object.hasOwn(value, "_default")) {
           return value["_default"];
         }
         return state[key];
       };
+      selectors[createSelectorName(key)] = selectorFunction;
     }
   }
 
