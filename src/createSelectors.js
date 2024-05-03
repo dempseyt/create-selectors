e91c2d0efc25c7612b7f1627d743408f05d4bf67
dempseyt <dempsey.thompson@journeyone.com.au>
2024-05-03 13:49:40 +0800

    work in progress

--- src/createSelectors.js
+++ src/createSelectors.js
@@ -1,11 +1,44 @@
-import R from "ramda";
+import R, { prop } from "ramda";
 
-const RESERVED_WORDS = ["_default", "_type", "_export", "_selector"];
+const RESERVED_WORDS = [
+  "_default",
+  "_type",
+  "_export",
+  "_selector",
+  "_alternative",
+];
 
-function createSelectorName(selectorName) {
-  return `select${selectorName.charAt(0).toUpperCase()}${selectorName.slice(
-    1
-  )}`;
+const createStateSelector = (selectorSpec) => {
+  return {
+    selectState: selectorSpec._selector ?? R.identity,
+  };
+};
+
+function createSelectorName(
+  propertyName,
+  propertySelectorSpec,
+  previousSelectorNames
+) {
+  let _createSelectorName = (propertyName) =>
+    `select${propertyName.charAt(0).toUpperCase()}${propertyName.slice(1)}`;
+
+  const selectorName = _createSelectorName(propertyName);
+
+  if (previousSelectorNames.includes(propertyName)) {
+    if (Object.hasOwn(propertySelectorSpec, "_alternative")) {
+      return _createSelectorName(propertySelectorSpec["_alternative"]);
+    }
+    throw new Error(
+      `Invariant failed: The selector names [${selectorName}] are already in use. Please use an alternative name using '_name' or '_names'`
+    );
+  }
+  return selectorName;
+}
+
+function throwInvariantErrorMsg(selectorName) {
+  throw new Error(
+    `Invariant failed: The selector names [${selectorName}] are already in use. Please use an alternative name using '_name' or '_names'`
+  );
 }
 
 function getDefaultValueForType(type) {
@@ -23,50 +56,50 @@ function getDefaultForPropertySelector(propertySelectorSpec) {
     return getDefaultValueForType(propertySelectorSpec["_type"]);
   }
 }
-
-function _createSelectors(selectorSpec, prevSelectorNames) {
-  const selectors = {
-    selectState: selectorSpec._selector ?? R.identity,
-  };
+function expandSelectors(
+  selectorSpec,
+  selectors = {
+    withOneName: [],
+    withAlternativeName: [],
+  }
+) {
+  const selectState = createStateSelector(selectorSpec);
 
   return Object.entries(selectorSpec).reduce(
     (selectors, [propertyName, propertySpec]) => {
       if (RESERVED_WORDS.includes(propertyName)) {
         return selectors;
       } else if (propertySpec._export !== false) {
-        const selectorName = createSelectorName(propertyName);
-
-        if (prevSelectorNames.includes(propertyName)) {
-          throw new Error(
-            `Invariant failed: The selector names [${selectorName}] are already in use. Please use an alternative name using '_name' or '_names'`
-          );
-        }
-
         const defaultValue = getDefaultForPropertySelector(
           selectorSpec[propertyName]
         );
 
         const selector = (_state) => {
-          const state = selectors.selectState(_state);
-          return Object.hasOwn(state, propertyName) &&
+          const state = selectState(_state);
+
+          return state[propertyName] !== undefined &&
+            Object.hasOwn(state, propertyName) &&
             state[propertyName] !== undefined
             ? state[propertyName]
             : defaultValue;
         };
 
-        selectors[selectorName] = selector;
-        prevSelectorNames.push(propertyName);
+        if (Object.hasOwn(propertySpec, "_alternative")) {
+          withAlternativeName.push({
+            name: [propertyName, propertySpec._alternative],
+            propertySelector: selector,
+          });
+        } else {
+          withOneName.push({
+            name: propertyName,
+            propertySelector: selector,
+          });
+        }
 
-        return {
-          ...selectors,
-          ..._createSelectors(
-            {
-              ...propertySpec,
-              _selector: selector,
-            },
-            prevSelectorNames
-          ),
-        };
+        return expandSelectors({
+          ...propertySpec,
+          _selector: selector,
+        });
       }
       return selectors;
     },
@@ -75,7 +108,21 @@ function _createSelectors(selectorSpec, prevSelectorNames) {
 }
 
 function createSelectors(selectorSpec) {
-  return _createSelectors(selectorSpec, []);
+  const selectors = { selectState: createStateSelector(selectorSpec) };
+  const selectorsWithMethodNames = expandSelectors(selectorSpec);
+
+  const reducer = (selectorsWithMethodNames, {names, propertySpec: propertySelector}) => {
+    const selectorNames = selectorsWithMethodNames.map((name) => createSelectorName(name))
+    for (const selectorName of selectorNames) {
+      if (!Object.hasOwn(selectorsWithMethodNames, selectorName)) {
+        selectorsWithMethodNames[selectorName] = propertySelector
+        return selectorsWithMethodNames
+      }
+    }
+  }
+  throwInvariantErrorMsg(selectorNames)
+
+  const 
 }
 
 export default createSelectors;
